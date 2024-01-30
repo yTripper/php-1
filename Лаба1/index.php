a7c99727d08fa4685bc38f252a78f66d847ba112
yTripper <149474382+yTripper@users.noreply.github.com>
2024-01-30 17:10:26 +0300

    Второй коммит

--- Лаба1/index.php
+++ Лаба1/index.php
@@ -6,11 +6,21 @@
         <title>Document</title>
     </head>
     <body>
-        <h1>
             <?php
-            echo "Hello, PHP"
+            // echo "<h1> Hello, PHP </h1>";
+            $a = 10;
+            $b = "20";
+            define("pi", 3.14);
+            // $x = $a * pi;
+            // echo $a+$b;
+            // $a = (int)$b;
+            // echo gettype ($a);
+            // echo "<h2>$a</h2>";
+            $x = 3.4;
+            $z = 5.8;
+            echo '$x = '.floor($x).'$z = '.floor($z).'<br>';
+            echo '$x = '.round($x).'$z = '.round($z).'<br>';
+            echo '$x = '.ceil($x).'$z = '.ceil($z).'<br>';
             ?>
-        </h1>
-
 </body>
 </html>
