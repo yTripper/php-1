94d20225eeb47d469d6fbf51252722447689348e
yTripper <149474382+yTripper@users.noreply.github.com>
2024-01-30 16:30:16 +0300

    first commit

---
+++ Лаба1/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF=8">
+        <meta name="viewport" content="">
+        <title>Document</title>
+    </head>
+    <body>
+        <h1>
+            <?php
+            echo "Hello, PHP"
+            ?>
+        </h1>
+
+</body>
+</html>
