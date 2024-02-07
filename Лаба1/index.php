4e6a069bc2b2745f53d0f4b508112531a206c84a
yTripper <149474382+yTripper@users.noreply.github.com>
2024-02-07 10:37:13 +0300

    Третий коммит

--- Лаба1/index.php
+++ Лаба1/index.php
@@ -2,25 +2,43 @@
 <html lang="en">
     <head>
         <meta charset="UTF=8">
-        <meta name="viewport" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Document</title>
     </head>
     <body>
+        <header>
+            <img src="logo.jpg" alt="logotip" class="logo">
+            <h1>Домашняя работа - Hello, World! </h1>
+        </header>
+        <h2>
             <?php
-            // echo "<h1> Hello, PHP </h1>";
-            $a = 10;
-            $b = "20";
-            define("pi", 3.14);
+            echo "<h1> Hello, PHP </h1>";
+            ?>
+        </h2>
+        <h3>
+            <?php
+            // $a = 10;
+            // $b = "20";
+            // define("pi", 3.14);
             // $x = $a * pi;
             // echo $a+$b;
             // $a = (int)$b;
             // echo gettype ($a);
             // echo "<h2>$a</h2>";
-            $x = 3.4;
-            $z = 5.8;
-            echo '$x = '.floor($x).'$z = '.floor($z).'<br>';
-            echo '$x = '.round($x).'$z = '.round($z).'<br>';
-            echo '$x = '.ceil($x).'$z = '.ceil($z).'<br>';
+            // $x = 3.4;
+            // $z = 5.8;
+            // echo '$x = '.floor($x).'$z = '.floor($z).'<br>';
+            // echo '$x = '.round($x).'$z = '.round($z).'<br>';
+            // echo '$x = '.ceil($x).'$z = '.ceil($z).'<br>';
+            // print '2<sup>10</sup> ='
+
+
+
+            // if ($a == 100): echo $a = 'ok';
+            //     else echo $a = 'no';
+            // 
+            // echo ($a == 100)? 'ok':'no'; 
             ?>
+        <h3>
 </body>
 </html>
