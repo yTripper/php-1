85e69b90262ba8a52bb6b1f66712dab367b79354
yTripper <149474382+yTripper@users.noreply.github.com>
2024-02-13 13:25:17 +0300

    Коммит

--- Лаба1/index.php
+++ Лаба1/index.php
@@ -4,17 +4,34 @@
         <meta charset="UTF=8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Document</title>
+        <style>
+            header {
+                display: flex;
+            }
+            h1 {
+                margin: auto;
+                justify-content: center;
+            }
+            h2 {
+                margin: auto;
+                justify-content: center;
+                margin-top: 4rem;
+            }
+        </style>
     </head>
     <body>
         <header>
-            <img src="logo.jpg" alt="logotip" class="logo">
+            <img src="logo.jpg" alt="logotip" class="logo" height="100rem">
             <h1>Домашняя работа - Hello, World! </h1>
         </header>
+        <main>
         <h2>
             <?php
-            echo "<h1> Hello, PHP </h1>";
+            echo 'Hello, PHP';
             ?>
         </h2>
+        </main>
+        <footer>
         <h3>
             <?php
             // $a = 10;
@@ -31,14 +48,12 @@
             // echo '$x = '.round($x).'$z = '.round($z).'<br>';
             // echo '$x = '.ceil($x).'$z = '.ceil($z).'<br>';
             // print '2<sup>10</sup> ='
-
-
-
             // if ($a == 100): echo $a = 'ok';
             //     else echo $a = 'no';
             // 
             // echo ($a == 100)? 'ok':'no'; 
             ?>
         <h3>
+        </footer>
 </body>
 </html>
