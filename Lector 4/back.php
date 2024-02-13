fb9368ef7ea1b6abaf47d85299548c9b05b39b43
yTripper <149474382+yTripper@users.noreply.github.com>
2024-02-13 15:37:27 +0300

    First commit

---
+++ Lector 4/back.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF=8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+        <?php
+            get_headers('https://github.com/yTripper/php-1/tree/main')
+        ?>
+        <a href="index.html">Переход на 1 страницу</a>
+    </body>
+</html>
